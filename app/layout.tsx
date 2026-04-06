24f83eb972a6a59f509f1b5a1b6bfd03acecf563
killiancolla <killiancolla@hotmail.fr>
2026-04-06 17:17:48 +0200

    Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Cormorant_Garamond, DM_Sans } from "next/font/google";
 import { getLocale } from "next-intl/server";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const cormorant = Cormorant_Garamond({
@@ -38,7 +39,10 @@ export default async function RootLayout({
       data-scroll-behavior="smooth"
       className={`${cormorant.variable} ${dmSans.variable}`}
     >
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
